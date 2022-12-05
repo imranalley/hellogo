73ab0ca4536e4cc85cad01ff2dac9a1b0691fba4
Imran Ali <imranrali1@gmail.com>
2022-12-05 12:10:00 -0500

    learning go

---
+++ hello.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
+
+func main() {
+	fmt.Println(quote.Go())
+	fmt.Println(quote.Hello())
+	fmt.Println(quote.Opt())
+	fmt.Println(quote.Glass())
+}
